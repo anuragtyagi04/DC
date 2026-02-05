6188771fb99cfaf495bee2073a6fbe18c2d1dc05
pm-andy <61549412+pm-andy@users.noreply.github.com>
2026-02-05 19:34:32 +0100

    first draft single page top

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-console.log("Site loaded");
+const nav = document.getElementById("site-nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    nav.classList.add("is-scrolled");
+  } else {
+    nav.classList.remove("is-scrolled");
+  }
+});
