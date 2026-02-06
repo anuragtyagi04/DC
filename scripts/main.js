6dd99696604c7bd860800123cbd3e088702bfefa
pm-andy <61549412+pm-andy@users.noreply.github.com>
2026-02-06 15:51:37 +0100

    ADDED more sections

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,18 @@ burger.addEventListener("click", () => {
   menu.classList.toggle("is-open");
   burger.classList.toggle("is-open");
 });
+
+const placeholder = document.getElementById("video-placeholder");
+
+  placeholder.addEventListener("click", () => {
+    placeholder.innerHTML = `
+      <iframe
+        src="https://www.youtube.com/embed/du-TY1GUFGk?autoplay=1"
+        title="Showreel"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowfullscreen
+        style="width:100%; height:100%; border-radius:5px;"
+      ></iframe>
+    `;
+  });
